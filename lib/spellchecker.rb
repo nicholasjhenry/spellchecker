acdc9305a8a919ed23d442853550cf06dfd85d3e
Nicholas Henry <nicholas.henry@gmail.com>
2013-09-11 15:10:58 -0400

    Rename method clarifying intent

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -25,7 +25,7 @@ class Spellchecker
     results = spell_check_response.split("\n").slice(1..-1)
     result_index = 0
     response.each_with_index do |word_hash, index|
-      build_response(response, word_hash, index, results, result_index)
+      build_response_element(response, word_hash, index, results, result_index)
     end
 
     response
@@ -48,7 +48,7 @@ class Spellchecker
     text.split(' ').collect { |original| {:original => original} }
   end
 
-  def self.build_response(response, word_hash, index, results, result_index)
+  def self.build_response_element(response, word_hash, index, results, result_index)
     if word_hash[:original] =~ /[a-zA-z\[\]\?]/
       if results[result_index] =~ ASPELL_WORD_DATA_REGEX
         suggestions = results[result_index].split(':')[1].strip.split(',').map(&:strip)
