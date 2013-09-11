51318a275666dbfc9ac6964da9e450a2233649f8
Nicholas Henry <nicholas.henry@gmail.com>
2013-09-11 15:18:29 -0400

    Extract command object to build response

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -37,31 +37,45 @@ class Spellchecker
   end
 
   def self.build_response(text, spell_check_response)
-    response = extract_original_string_tokens(text)
-    results = spell_check_response.split("\n").slice(1..-1)
-    result_index = 0
-    response.each_with_index do |word_hash, index|
-      build_response_element(response, word_hash, index, results, result_index)
+    ResponseBuilder.new(text, spell_check_response).to_hash
+  end
+
+  class ResponseBuilder
+    def initialize(text, spell_check_response)
+      @text, @spell_check_response = text, spell_check_response
     end
 
-    response
-  end
+    def to_hash
+      response = extract_original_string_tokens(text)
+      results = spell_check_response.split("\n").slice(1..-1)
+      result_index = 0
+      response.each_with_index do |word_hash, index|
+        build_response_element(response, word_hash, index, results, result_index)
+      end
 
-  def self.extract_original_string_tokens(text)
-    text.split(' ').collect { |original| {:original => original} }
-  end
+      response
+    end
 
-  def self.build_response_element(response, word_hash, index, results, result_index)
-    if word_hash[:original] =~ /[a-zA-z\[\]\?]/
-      if results[result_index] =~ ASPELL_WORD_DATA_REGEX
-        suggestions = results[result_index].split(':')[1].strip.split(',').map(&:strip)
-        response[index].merge!(:correct => false, :suggestions => suggestions)
+    private
+
+    attr_reader :text, :spell_check_response
+
+    def extract_original_string_tokens(text)
+      text.split(' ').collect { |original| {:original => original} }
+    end
+
+    def build_response_element(response, word_hash, index, results, result_index)
+      if word_hash[:original] =~ /[a-zA-z\[\]\?]/
+        if results[result_index] =~ ASPELL_WORD_DATA_REGEX
+          suggestions = results[result_index].split(':')[1].strip.split(',').map(&:strip)
+          response[index].merge!(:correct => false, :suggestions => suggestions)
+        else
+          response[index].merge!(:correct => true)
+        end
+        result_index += 1
       else
         response[index].merge!(:correct => true)
       end
-      result_index += 1
-    else
-      response[index].merge!(:correct => true)
     end
   end
 end
