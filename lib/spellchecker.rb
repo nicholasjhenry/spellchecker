67517a5f1f4ebcb530497eb4d32b99e596abb0d0
Nicholas Henry <nicholas.henry@gmail.com>
2013-09-11 15:23:44 -0400

    Move BuildResponse into separate file

--- lib/spellchecker.rb
+++ lib/spellchecker.rb
@@ -1,8 +1,11 @@
+$:.unshift File.expand_path('../../lib', __FILE__)
+
 class Spellchecker
   require 'net/https'
   require 'uri'
   require 'rexml/document'
   require 'open3'
+  require 'spellchecker/build_response'
 
   ASPELL_WORD_DATA_REGEX = Regexp.new(/\&\s\w+\s\d+\s\d+(.*)$/)
 
@@ -37,45 +40,6 @@ class Spellchecker
   end
 
   def self.build_response(text, spell_check_response)
-    ResponseBuilder.new(text, spell_check_response).to_hash
-  end
-
-  class ResponseBuilder
-    def initialize(text, spell_check_response)
-      @text, @spell_check_response = text, spell_check_response
-    end
-
-    def to_hash
-      response = extract_original_string_tokens(text)
-      results = spell_check_response.split("\n").slice(1..-1)
-      result_index = 0
-      response.each_with_index do |word_hash, index|
-        build_response_element(response, word_hash, index, results, result_index)
-      end
-
-      response
-    end
-
-    private
-
-    attr_reader :text, :spell_check_response
-
-    def extract_original_string_tokens(text)
-      text.split(' ').collect { |original| {:original => original} }
-    end
-
-    def build_response_element(response, word_hash, index, results, result_index)
-      if word_hash[:original] =~ /[a-zA-z\[\]\?]/
-        if results[result_index] =~ ASPELL_WORD_DATA_REGEX
-          suggestions = results[result_index].split(':')[1].strip.split(',').map(&:strip)
-          response[index].merge!(:correct => false, :suggestions => suggestions)
-        else
-          response[index].merge!(:correct => true)
-        end
-        result_index += 1
-      else
-        response[index].merge!(:correct => true)
-      end
-    end
+    BuildResponse.new(text, spell_check_response).call
   end
 end
---
+++ lib/spellchecker/build_response.rb
@@ -0,0 +1,40 @@
+class Spellchecker
+  class BuildResponse
+    def initialize(text, spell_check_response)
+      @text, @spell_check_response = text, spell_check_response
+    end
+
+    def call
+      response = extract_original_string_tokens(text)
+      results = spell_check_response.split("\n").slice(1..-1)
+      result_index = 0
+      response.each_with_index do |word_hash, index|
+        build_response_element(response, word_hash, index, results, result_index)
+      end
+
+      response
+    end
+
+    private
+
+    attr_reader :text, :spell_check_response
+
+    def extract_original_string_tokens(text)
+      text.split(' ').collect { |original| {:original => original} }
+    end
+
+    def build_response_element(response, word_hash, index, results, result_index)
+      if word_hash[:original] =~ /[a-zA-z\[\]\?]/
+        if results[result_index] =~ ASPELL_WORD_DATA_REGEX
+          suggestions = results[result_index].split(':')[1].strip.split(',').map(&:strip)
+          response[index].merge!(:correct => false, :suggestions => suggestions)
+        else
+          response[index].merge!(:correct => true)
+        end
+        result_index += 1
+      else
+        response[index].merge!(:correct => true)
+      end
+    end
+  end
+end
