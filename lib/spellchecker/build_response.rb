96c08aa37c54ce8b2c6259019928347891fae726
Nicholas Henry <nicholas.henry@gmail.com>
2013-09-12 09:12:55 -0400

    Reduce parameter complexity

--- lib/spellchecker/build_response.rb
+++ lib/spellchecker/build_response.rb
@@ -12,7 +12,7 @@ class Spellchecker
       result_index = 0
 
       response.each_with_index do |word_hash, index|
-        build_response_element(response, word_hash, index, results, result_index)
+        build_response_element(response[index], word_hash, results, result_index)
       end
 
       response
@@ -30,17 +30,17 @@ class Spellchecker
       command_output.split("\n").slice(1..-1)
     end
 
-    def build_response_element(response, word_hash, index, results, result_index)
+    def build_response_element(response_element, word_hash, results, result_index)
       if valid_word?(word_hash[:original])
         if correct_spelling?(results[result_index])
-          response[index].merge!(:correct => true)
+          response_element.merge!(:correct => true)
         else
           suggestions = extract_suggestions(results[result_index])
-          response[index].merge!(:correct => false, :suggestions => suggestions)
+          response_element.merge!(:correct => false, :suggestions => suggestions)
         end
         result_index += 1
       else
-        response[index].merge!(:correct => true)
+        response_element.merge!(:correct => true)
       end
     end
 
